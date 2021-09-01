0af52d8cad447af395aa773960517f8aafa0d9f2
UmarSarfaraz <89626401+UmarSarfaraz@users.noreply.github.com>
2021-09-01 22:20:55 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+import Cards  from "./Cards";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+ReactDOM.render(
+    <>
+    <h1 className="heading">List of Top 5 Netflix Series</h1>
+    <Cards 
+      imgsrc="https://wallpapercave.com/wp/wp4056410.jpg"
+      title="Netflix Original Series"
+      sname="DARK"
+      link="https://www.netflix.com/pk/title/80100172"
+    />
+
+<Cards 
+      imgsrc="https://wallpapercave.com/wp/wp1917154.jpg"
+      title="Netflix Original Series"
+      sname="Stranger Things"
+      link="https://www.netflix.com/in/title/80990668?source=35"
+    />
+
+<Cards 
+      imgsrc="https://upload.wikimedia.org/wikipedia/en/e/ec/Save_Me_Poster.jpg"
+      title="Netflix Original Series"
+      sname="Save Me"
+      link="https://www.netflix.com/pk/title/81167101"
+    />
+
+<Cards 
+      imgsrc="https://images.justwatch.com/poster/8589251/s592"
+      title="Netflix Original Series"
+      sname="the Vampire diaries"
+      link="https://www.netflix.com/in/title/80990668?source=35"
+    />
+
+<Cards 
+      imgsrc="https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg"
+      title="Netflix Original Series"
+      sname="My First-2 Love"
+      link="https://www.netflix.com/pk/title/81026700"
+    />
+
+
+
+    </>,
+    document.getElementById("root")
+);
